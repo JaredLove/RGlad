0ec13d47bd029eb1f36a0327e3b8a98da0b0475f
JaredLove <lovejared91@Yahoo.com>
2024-06-22 19:53:51 -0500

    added enemy array with the function to loop through for combat

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,9 @@
+// Game States
+// "WIN" - Player robot has defeated all enemy-robots 
+//      * Fight all enemy-robots
+//      * Defeat each enemy-robot
+// "LOSE" - Player robot's health is zero or less
+
 let playerName = window.prompt("What is your robots name?");
 let playerHealth = 100;
 let playerAttack = 10;
@@ -8,52 +14,63 @@ let playerMoney = 10;
 console.log(`${playerName}'s specs: Health: ${playerHealth} Attack: ${playerAttack}`);
 
 
-let enemyName = "Roborto";
+const enemyRobots = ["Roborto", "Amy Andriod", "Robo Trumble"];
 let enemyHealth = 50;
 let enemyAttack = 12;
 
-console.log(`${enemyName}'s specs: Health: ${enemyHealth} Attack: ${enemyAttack}`);
+// console.log(`${enemyRobot}'s specs: Health: ${enemyHealth} Attack: ${enemyAttack}`);
     // alert players the round started
     window.alert("Welcome To Robot Gladiators");
 
-const fight = function(){
+const fight = function(enemyName){
+    while(playerHealth > 0 || enemyHealth > 0){
+        let promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose");
 
-    let promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose");
 
-
-    if(promptFight === "FIGHT" || promptFight === "fight"){
-        // subtract the value of playerAttack from the value of enemyHealth
-        enemyHealth -= playerAttack;
-        if (enemyHealth <= 0){
-            window.alert(`${playerName} has done ${playerAttack} damage to ${enemyName} resulting in his death!`)
-        }else {
-            // log the result
-            console.log(`${playerName} attacked ${enemyName} for ${playerAttack} damage and ${enemyName} now has ${enemyHealth} health left.`);
-        }
-        // subtract the enemyAttack from the playerHealth
-        playerHealth -= enemyAttack;
-        if (playerHealth <= 0){
-            window.alert(`${enemyName} has done ${enemyAttack} damage to ${playerName} resulting in his death!`)
+        if(promptFight === "FIGHT" || promptFight === "fight"){
+            // subtract the value of playerAttack from the value of enemyHealth
+            enemyHealth -= playerAttack;
+            if (enemyHealth <= 0){
+                window.alert(`${playerName} has done ${playerAttack} damage to ${enemyName} resulting in his death!`);
+                return;
+            }else {
+                // log the result
+                console.log(`${playerName} attacked ${enemyName} for ${playerAttack} damage and ${enemyName} now has ${enemyHealth} health left.`);
+            }
+            // subtract the enemyAttack from the playerHealth
+            playerHealth -= enemyAttack;
+            if (playerHealth <= 0){
+                window.alert(`${enemyName} has done ${enemyAttack} damage to ${playerName} resulting in his death!`);
+                return;
+            }else{
+                // log the result 
+                console.log(`${enemyName} attacked ${playerName} for ${enemyAttack} damage and ${playerName} now has ${playerHealth} health left.`);
+            }
+        }else if(promptFight === "SKIP" || promptFight === "skip"){
+            window.alert(`You chose to skip!`);
+            window.alert(`The bosses arent happy about this, they are going to take away some of your prize money!`);
+            let confirm = window.confirm(`Are you sure you want to do this?`);
+            if(confirm){
+                playerMoney -= 2;
+                window.alert(`The bosses took 2 gold pieces from you!`)
+                fight();
+            }else{
+                window.alert(`That the spirit ${playerName}, get back in there!`);
+                fight();
+            }
         }else{
-            // log the result 
-            console.log(`${enemyName} attacked ${playerName} for ${enemyAttack} damage and ${playerName} now has ${playerHealth} health left.`);
-        }
-    }else if(promptFight === "SKIP" || promptFight === "skip"){
-        window.alert(`You chose to skip!`);
-        window.alert(`The bosses arent happy about this, they are going to take away some of your prize money!`);
-        let confirm = window.confirm(`Are you sure you want to do this?`);
-        if(confirm){
-            playerMoney -= 2;
-            window.alert(`The bosses took 2 gold pieces from you!`)
-        }else{
-            window.alert(`That the spirit ${playerName}, get back in there!`);
+            window.alert("Please input a valid option 'FIGHT' or 'SKIP'");
             fight();
         }
-    }else{
-        window.alert("Please input a valid option 'FIGHT' or 'SKIP'");
-        fight();
     }
 };
 
+const startGame = function(enemyNames){ 
+    for(i=0; i < enemyNames.length; i++){
+        fight(enemyNames[i]);
+        enemyHealth = 50;
+        playerHealth = 100;
+    }
+}
 
-fight();
+startGame(enemyRobots);
