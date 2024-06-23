b1e5dd404e089391fd2c1303deb454daf5d734c7
JaredLove <lovejared91@Yahoo.com>
2024-06-22 20:14:48 -0500

    added a quit option and money deduction for skipping the fight

--- assets/js/game.js
+++ assets/js/game.js
@@ -23,8 +23,8 @@ let enemyAttack = 12;
     window.alert("Welcome To Robot Gladiators");
 
 const fight = function(enemyName){
-    while(playerHealth > 0 || enemyHealth > 0){
-        let promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose");
+    while(playerHealth > 0 && enemyHealth > 0){
+        let promptFight = window.prompt("Would you like to FIGHT, SKIP, or End this battle? Enter 'FIGHT', 'SKIP', or 'Quit' to choose");
 
 
         if(promptFight === "FIGHT" || promptFight === "fight"){
@@ -32,7 +32,7 @@ const fight = function(enemyName){
             enemyHealth -= playerAttack;
             if (enemyHealth <= 0){
                 window.alert(`${playerName} has done ${playerAttack} damage to ${enemyName} resulting in his death!`);
-                return;
+                break;
             }else {
                 // log the result
                 console.log(`${playerName} attacked ${enemyName} for ${playerAttack} damage and ${enemyName} now has ${enemyHealth} health left.`);
@@ -40,25 +40,30 @@ const fight = function(enemyName){
             // subtract the enemyAttack from the playerHealth
             playerHealth -= enemyAttack;
             if (playerHealth <= 0){
-                window.alert(`${enemyName} has done ${enemyAttack} damage to ${playerName} resulting in his death!`);
-                return;
+                window.alert(`${enemyName} has done ${enemyAttack} damage to ${playerName} resulting in your death!`);
+                break;
             }else{
                 // log the result 
                 console.log(`${enemyName} attacked ${playerName} for ${enemyAttack} damage and ${playerName} now has ${playerHealth} health left.`);
             }
         }else if(promptFight === "SKIP" || promptFight === "skip"){
             window.alert(`You chose to skip!`);
-            window.alert(`The bosses arent happy about this, they are going to take away some of your prize money!`);
+            window.alert(`The bosses arent happy about this!`);
             let confirm = window.confirm(`Are you sure you want to do this?`);
             if(confirm){
                 playerMoney -= 2;
                 window.alert(`The bosses took 2 gold pieces from you!`)
+                console.log(`Your now have ${playerMoney} gold pieces left.`)
                 fight();
             }else{
                 window.alert(`That the spirit ${playerName}, get back in there!`);
                 fight();
             }
-        }else{
+        }else if(promptFight === "QUIT" || promptFight === "quit"){
+            window.alert(`${playerName} has chosen to quit the game!`);
+            window.alert(`Come back when you have to guts to win ${playerName}!`);
+            break;
+        }else{  
             window.alert("Please input a valid option 'FIGHT' or 'SKIP'");
             fight();
         }
@@ -67,9 +72,11 @@ const fight = function(enemyName){
 
 const startGame = function(enemyNames){ 
     for(i=0; i < enemyNames.length; i++){
-        fight(enemyNames[i]);
+        let pickedEnemyName = enemyNames[i];        
         enemyHealth = 50;
         playerHealth = 100;
+        fight(pickedEnemyName);
+
     }
 }
 
